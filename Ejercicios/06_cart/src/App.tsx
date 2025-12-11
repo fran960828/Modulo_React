616594e96fdcc6ddb4a0fd1d7f16502c2b7e074e
Francisco Navarro <franng96@hotmail.com>
2025-12-11 14:44:20 +0100

    tail

--- Ejercicios/06_cart/src/App.tsx
+++ Ejercicios/06_cart/src/App.tsx
@@ -8,27 +8,37 @@ function App() {
     <>
       <div id="container" className="my-[3%] mx-[15%] px-4 py-2">
         <Header />
-        <main>
-          <h2>ELEGANT CLOTHING FOR EVERYONE</h2>
-          <section className="flex flex-row flex-wrap gap-8 ">
+        <section>
+          <h2 className="text-2xl text-stone-400 uppercase font-bold mb-4">
+            ELEGANT CLOTHING FOR EVERYONE
+          </h2>
+          <ul className="grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-8 list-none">
             {DUMMY_PRODUCTS.map((product: Iproduct) => (
-              <li>
-                <article
-                  key={product.id}
-                  className="flex flex-col gap-1 w-1/4 bg-[#5f4e33]"
-                >
-                  <img src={product.image} alt="imagen" />
-                  <div id="product__button">
-                    <h3>{product.title}</h3>
-                    <p>${product.price}</p>
-                    <p>{product.description}</p>
+              <li key={product.id} className="m-0 p-0">
+                <article className="flex flex-col h-full bg-[#5f4e33] shadow-[0_0_10px_rgba(0,0,0,0.2)] rounded-md">
+                  <img
+                    src={product.image}
+                    alt="imagen"
+                    className="mb-2 rounded-md"
+                  />
+                  <div
+                    id="product__button"
+                    className="flex-1 p-1 flex flex-col justify-between"
+                  >
+                    <h3 className="font-bold text-xl text-[#fbd392] ">
+                      {product.title}
+                    </h3>
+                    <p className="text-sm mb-2 text-[#fbd392]">
+                      ${product.price}
+                    </p>
+                    <p className="mb-8 text-sm ">{product.description}</p>
                     <Button>Add Cart</Button>
                   </div>
                 </article>
               </li>
             ))}
-          </section>
-        </main>
+          </ul>
+        </section>
       </div>
     </>
   );
--- Ejercicios/06_cart/src/components/Button.tsx
+++ Ejercicios/06_cart/src/components/Button.tsx
@@ -2,7 +2,7 @@ import type { Ibutton } from "../core_modules/core";
 
 export function Button({ children }: Ibutton) {
   return (
-    <button className="px-8 py-2 rounded-sm bg-[#edbf68] text-xl font-bold text-stone-700">
+    <button className="px-8 py-2 rounded-sm bg-[#edbf68] text-xl font-bold text-stone-700 hover:text-[#edbf68] hover:bg-stone-700">
       {children}
     </button>
   );
